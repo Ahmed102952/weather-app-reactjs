15b08d22fcab8ef57ab3101284305bcb8e5b399c
Ahmed <ahmeda8400@gmail.com>
2022-10-26 09:41:31 +0200

    .

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,31 +1,48 @@
-import { dateT, weatherDataT } from '../App'
-import { CityName, Flex, P, Temp, Title, WeatherDisplayDiv, WeatherInfo } from '../app.stayle'
+import { dateT, weatherDataT } from "../App";
+import {
+  CityName,
+  Flex,
+  P,
+  Temp,
+  Title,
+  WeatherDisplayDiv,
+  WeatherInfo,
+} from "../app.stayle";
 
-type WeatherDisplayProps = Partial<weatherDataT & dateT>
-const WeatherDisplay = ({temp_c,name,hour,min,dayName,day,monthName,year,text}: WeatherDisplayProps) => {
+type WeatherDisplayProps = Partial<weatherDataT & dateT>;
+const WeatherDisplay = ({
+  temp_c,
+  name,
+  hour,
+  min,
+  dayName,
+  day,
+  monthName,
+  year,
+  text,
+}: WeatherDisplayProps) => {
   return (
     <WeatherDisplayDiv>
-        <Title>The Weather</Title>
-        <WeatherInfo>
-          <Temp>{temp_c?.toFixed()}&deg;</Temp>
-          <div>
-            <CityName>{name}</CityName>
-            <span>
-              {hour}:{min}-{dayName}, {day} {monthName}{" "}
-              &apos;{year}
-            </span>
-          </div>
-          <Flex flex_column ai_c>
-            <img
-              style={{ width: "auto", height: "auto" }}
-              src="https://cdn.weatherapi.com/weather/64x64/day/113.png"
-              alt="icon"
-            />
-            <P>{text}</P>
-          </Flex>
-        </WeatherInfo>
-      </WeatherDisplayDiv>
-  )
-}
+      <Title>The Weather</Title>
+      <WeatherInfo>
+        <Temp>{temp_c?.toFixed()}&deg;</Temp>
+        <div>
+          <CityName>{name}</CityName>
+          <span>
+            {hour}:{min}-{dayName}, {day} {monthName} &apos;{year}
+          </span>
+        </div>
+        <Flex flex_column ai_c>
+          <img
+            style={{ width: "35px", height: "35px" }}
+            src="https://cdn.weatherapi.com/weather/64x64/day/113.png"
+            alt="icon"
+          />
+          <P style={{ width: "35px", height: "35px" }}>{text}</P>
+        </Flex>
+      </WeatherInfo>
+    </WeatherDisplayDiv>
+  );
+};
 
-export default WeatherDisplay
+export default WeatherDisplay;
